fd16f35f9b09f3e6458513e4352114738e5e8587
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-21 14:32:16 +0400

    unzip installation on docker

--- run-scripts.sh
+++ run-scripts.sh
@@ -1,8 +1,13 @@
 cp /github-pr-reviews.main.kts $GITHUB_WORKSPACE
 cp /ktlint $GITHUB_WORKSPACE
 
+echo 'installing unzip'
+Sudo apt-get install unzip
+echo 'unzip installation done'
+
 echo 'installing sdk man'
 curl -s "https://get.sdkman.io" | bash
+echo 'sdkman installation done'
 
 echo 'Starting main script file...'
 kotlinc -script ./github-pr-reviews.main.kts $INPUT_EVENT_FILE_PATH $INPUT_GITHUB_TOKEN
