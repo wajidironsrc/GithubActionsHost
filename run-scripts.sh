e773bc5e03c5365e38a93d728a293d277621e682
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-21 14:21:39 +0400

    newwwest

--- run-scripts.sh
+++ run-scripts.sh
@@ -2,4 +2,4 @@ cp /github-pr-reviews.main.kts $GITHUB_WORKSPACE
 cp /ktlint $GITHUB_WORKSPACE
 
 echo 'Starting main script file...'
-./github-pr-reviews.main.kts $INPUT_EVENT_FILE_PATH $INPUT_GITHUB_TOKEN
+kotlinc -script ./github-pr-reviews.main.kts $INPUT_EVENT_FILE_PATH $INPUT_GITHUB_TOKEN
