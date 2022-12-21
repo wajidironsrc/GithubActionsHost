62563290d0404982cc32a92da987c2b45ac466f6
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-21 14:26:42 +0400

    sdkman installation

--- run-scripts.sh
+++ run-scripts.sh
@@ -1,5 +1,8 @@
 cp /github-pr-reviews.main.kts $GITHUB_WORKSPACE
 cp /ktlint $GITHUB_WORKSPACE
 
+echo 'installing sdk man'
+curl -s "https://get.sdkman.io" | bash
+
 echo 'Starting main script file...'
 kotlinc -script ./github-pr-reviews.main.kts $INPUT_EVENT_FILE_PATH $INPUT_GITHUB_TOKEN
