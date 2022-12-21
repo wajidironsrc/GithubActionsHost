a9710f353d320eed9eed56f7e480c5eea5b56d51
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-21 11:04:59 +0400

    change approach to docker

---
+++ run-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
+
+# cp /github-pr-reviews.main $GITHUB_WORKSPACE
+# cp /ktlint $GITHUB_WORKSPACE
+
+# echo 'Starting main script file...'
+# ./github-pr-reviews.main $GITHUB_EVENT_PATH $INPUT_REPOTOKEN
+
