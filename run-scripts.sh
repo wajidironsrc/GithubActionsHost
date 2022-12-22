dd9ce6bd7c123bb83f181959f12490c421f8f28d
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-22 10:48:47 +0400

    aaa

--- run-scripts.sh
+++ run-scripts.sh
@@ -1,5 +1,12 @@
 cp /github-pr-reviews.main.kts $GITHUB_WORKSPACE
 cp /ktlint $GITHUB_WORKSPACE
 
+echo 'kotlin version'
+kotlin --version
+
+echo 'kotlin help'
+kotlinc -help
+
+
 echo 'Starting main script file...'
 kotlinc --script ./github-pr-reviews.main.kts $INPUT_EVENT_FILE_PATH $INPUT_GITHUB_TOKEN
