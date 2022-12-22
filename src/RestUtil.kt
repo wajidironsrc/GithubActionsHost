48e324bb09ca2ca4abadc8db39a6cb8fa30267c8
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-22 23:09:25 +0400

    new changes

---
+++ src/RestUtil.kt
@@ -0,0 +1,51 @@
+import java.lang.System
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.HttpUrl
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.*
+import java.io.File
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+
+fun createRetrofit(): Retrofit {
+
+    val loggingInterceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+        override fun log(message: String) {
+            println("http: $message")
+        }
+    })
+
+    val client = OkHttpClient()
+        .newBuilder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    return Retrofit.Builder()
+        .baseUrl(HttpUrl.get("https://api.github.com"))
+        .addConverterFactory(MoshiConverterFactory.create(createMoshi()))
+        .client(client)
+        .build()
+}
+
+fun createMoshi(): Moshi {
+    return Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+}
+
+fun createGithubEventRequestModel(
+    eventFilePath: String
+): GithubEvent {
+
+    println("fun createGithubEvent: $eventFilePath")
+
+    val json = File(eventFilePath).readText()
+    return createMoshi()
+        .adapter(GithubEvent::class.java)
+        .fromJson(json)
+        ?: throw Exception("Could not create json from file: $eventFilePath")
+}
---
+++ src/github-pr-reviews.main.kts
@@ -0,0 +1,53 @@
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:DependsOn("com.squareup.moshi:moshi:1.9.3")
+@file:DependsOn("com.squareup.moshi:moshi-kotlin:1.9.3")
+@file:DependsOn("com.squareup.retrofit2:retrofit:2.9.0")
+@file:DependsOn("com.squareup.retrofit2:converter-moshi:2.9.0")
+@file:DependsOn("com.squareup.okhttp3:logging-interceptor:4.7.2")
+@file:Import("DataModels.kt")
+@file:Import("FetchPrChanges.kt")
+@file:Import("PostComments.kt")
+@file:Import("FetchExistingReviewComments.kt")
+@file:Import("RestUtil.kt")
+
+import java.io.File
+import kotlin.system.exitProcess
+
+val collectionReport = "collection-report.txt"
+val ktLintReport = "ktlint-report.json"
+val eventFilePath = args[0]
+val token = args[1]
+
+println("one: $eventFilePath")
+println("two: $token")
+exitProcess(0)
+
+//fetch PR changes
+val isSuccessFetchPrChanges = fetchPrChanges(
+    eventFilePath = eventFilePath,
+    token = token,
+    collectionReport
+)
+if (!isSuccessFetchPrChanges) {
+    exitProcess(-1)
+}
+
+//fetch PR comments
+val commentList = fetchAlreadyMadeComments(
+    eventFilePath = eventFilePath,
+    token = token
+)
+
+
+//post new comments if required any
+val isSuccessMakePrComments = makePrComments(
+    eventFilePath = eventFilePath,
+    token = token,
+    collectionReportPath = collectionReport,
+    ktlintReportPath = ktLintReport,
+    listOfAlreadyMadeComments = commentList
+)
+
+exitProcess(
+    if(isSuccessMakePrComments) 0 else -1
+)
