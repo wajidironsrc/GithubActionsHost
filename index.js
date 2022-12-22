b5051d8c7535ce0429c92657155d886280521cd6
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2022-12-22 16:57:47 +0400

    code refined

--- index.js
+++
@@ -1,5 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-
-console.log('this is from within actions');
--- package-script.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kscript --package .github/scripts/ktlint/github-pr-reviews.main.kts
