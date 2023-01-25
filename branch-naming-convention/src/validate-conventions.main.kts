c8a7cf1557a0867b1af4bc057b7d2a14f01cc247
wajidironsrc <114980264+wajidironsrc@users.noreply.github.com>
2023-01-25 10:01:24 +0400

    - more

--- branch-naming-convention/src/validate-conventions.main.kts
+++ branch-naming-convention/src/validate-conventions.main.kts
@@ -1,4 +1,5 @@
 @file:DependsOn("com.google.code.gson:gson:2.10")
+
 @file:Import("EventFileUtil.kt")
 
 import kotlin.system.exitProcess
@@ -10,9 +11,10 @@ val branchCompatibility = args[3] //Branch Name Compatibility
 val validateTicketsInCommit = args[4] //if true, commit msg against ticketFromCommitMessagePattern will be checked.
 val ticketFromCommitMessagePattern = args[5] //Regex, to verify commit messages with.
 
+println("Hey Its up and running")
 
 val eventFileUtil = EventFileUtil(eventFilePath)
 val githubEvent = eventFileUtil.getGithubEvent()
 println(Gson().toJson(githubEvent))
 
-exitProcess(1)
+exitProcess(0)
